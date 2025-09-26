e3070bf778078d2d669c6aee756127f803e3fc5e
Antonio Russo <antoniorusso1988@outlook.it>
2025-09-26 10:17:29 +0200

    Navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+
 const About = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate ("/")
+  };
   return (
     <div>
       <h1>About</h1>
       <p>Questa è una semplice applicazione To-Do creata con React e React Router.</p>
+      <button onClick={goHome}>Torna alla Home</button>
     </div>
   );
 };
